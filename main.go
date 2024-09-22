37c2aa00f28735f8fcdd56d2aaeda331a2b20874
Bulat Saifullin <bulatsajfullin00@gmail.com>
2024-09-23 02:37:08 +0300

    Update: integration with PostgreSQL, added task management functionality

--- main.go
+++ main.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
+	"errors"
 	"fmt"
-	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
+
+var db *sql.DB
+
+func handleError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %v", msg, err)
+	}
+}
+
+func initDB() {
+	var err error
+	connStr := "user=todo_user password=Nc0zt_oEa4cBLuT dbname=todo_db sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	handleError(err, "Ошибка с базой данных:")
+	err = db.Ping()
+	handleError(err, "Не удалось подключиться к базе данных:")
+	fmt.Println("Успешное подключение к базе данных!")
+}
+
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS tasks (
+	    id SERIAL PRIMARY KEY,
+	    name TEXT NOT NULL,
+	    status BOOLEAN NOT NULL DEFAULT FALSE
+	);`
+	_, err := db.Exec(query)
+	handleError(err, "Не удалось создать таблицу:")
+	fmt.Println("Таблица tasks создана или уже существует.")
+
+}
 
 func GetInputString() string {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -54,200 +88,129 @@ func menu() {
 	fmt.Println("3. Отметить выполнение задачи")
 	fmt.Println("4. Редактировать задачу")
 	fmt.Println("5. Показать все задачи")
-	fmt.Println("6. Загрузить данные")
-	fmt.Println("7. Сохранить данные")
-	fmt.Println("8. Выход")
-}
-
-type Task struct {
-	ID   string
-	Name string
-	//Description string
-	Status bool
-	//Priority    int
-	//Deadline    time.Time
-	//CreatedAt time.Time
-}
-
-var taskList []Task
-
-func getNextID() string {
-	return uuid.New().String()
+	fmt.Println("6. Выход")
 }
 
 func AddTask() {
 	fmt.Println("Добавление задачи")
-	fmt.Println("Введеите название задачи")
+	fmt.Println("Введите название задачи")
 	name := GetInputString()
-	fmt.Println(name)
-	newTask := Task{
-		ID:     getNextID(),
-		Name:   name,
-		Status: false,
-	}
-	taskList = append(taskList, newTask)
+	query := `
+	INSERT INTO tasks(name, status)
+	VALUES ($1, $2)`
+	_, err := db.Exec(query, name, false)
+	handleError(err, "Ошибка при добавлении задачи:")
 	fmt.Println("Задача успешно добавлена")
 }
 
 func DeleteTask() {
 	fmt.Println("Удаление задачи")
 	fmt.Println("Введите ID задачи")
-	id := GetInputString()
-	indexToRemove := -1
-	for i, task := range taskList {
-		if task.ID == id {
-			indexToRemove = i
-			break
-		}
-	}
-	if indexToRemove == -1 {
-		fmt.Printf("Задача с ID: %s не существует\n", id)
-	} else {
-		taskList = append(taskList[:indexToRemove], taskList[indexToRemove+1:]...)
-		fmt.Printf("Задача с ID: %s удалена\n", id)
-	}
+	id := GetInputInt()
+	query := `
+	DELETE FROM tasks 
+	WHERE id = $1`
+	_, err := db.Exec(query, id)
+	handleError(err, "Ошибка при удалении задачи:")
+	fmt.Printf("Задача с ID: %d удалена\n", id)
 }
 
 func MaskTask() {
 	fmt.Println("Отмечаем выполненную задачу")
 	fmt.Println("Введите ID задачи")
-	id := GetInputString()
-	indexToEdit := -1
-	for i, task := range taskList {
-		if task.ID == id {
-			indexToEdit = i
-			break
-		}
-	}
-	if indexToEdit == -1 {
-		fmt.Printf("Задача с ID: %s не существует\n", id)
-	} else {
-		taskList[indexToEdit].Status = true
-		fmt.Printf("Задача с ID: %s выполнена\n", id)
-	}
+	id := GetInputInt()
+	query := `
+	UPDATE tasks 
+	SET status = TRUE
+	WHERE id = $1`
+	_, err := db.Exec(query, id)
+	handleError(err, "Ошибка при обновлении задачи:")
+	fmt.Printf("Задача с ID: %d отмечена как выполнена\n", id)
 }
 
 func EditTask() {
 	fmt.Println("Редактируем задачу")
 	fmt.Println("Введите ID задачи")
-	id := GetInputString()
-	indexToEdit := -1
-	for i, task := range taskList {
-		if task.ID == id {
-			indexToEdit = i
-			break
-		}
+	id := GetInputInt()
+	var name string
+	var status bool
+	query := `
+	SELECT name, status FROM tasks 
+	WHERE id = $1
+	`
+	row := db.QueryRow(query, id)
+	err := row.Scan(&name, &status)
+	if errors.Is(err, sql.ErrNoRows) {
+		fmt.Printf("Задача с ID: %d не существует\n", id)
+		return
+	} else if err != nil {
+		log.Fatal("Ошибка при получении запроса:", err)
 	}
 
-	if indexToEdit == -1 {
-		fmt.Printf("Задача с ID: %s не существует\n", id)
-	} else {
-		fmt.Println("Выберите что хотите редактировать:")
-		fmt.Println("1. Name")
-		fmt.Println("2. Status")
-		choiceEdits := GetInputInt()
-		switch choiceEdits {
-		case 1:
-			fmt.Println("Редактируем имя")
-			newName := GetInputString()
-			taskList[indexToEdit].Name = newName
-			fmt.Printf("Задача с ID: %s редактирована\n", id)
-		case 2:
-			fmt.Println("Редактируем status")
-			fmt.Println("1. Выполнена")
-			fmt.Println("2. Не выполнена")
-			statusChoice := GetInputInt()
-			if statusChoice == 1 {
-				taskList[indexToEdit].Status = true
-			} else {
-				taskList[indexToEdit].Status = false
-			}
-			fmt.Printf("Задача с ID: %s редактирована\n", id)
-		default:
-			fmt.Println("Неверный выбор. Попробуй еще раз")
-		}
+	fmt.Println("Выберите что хотите редактировать:")
+	fmt.Println("1. Name")
+	fmt.Println("2. Status")
+	choiceEdits := GetInputInt()
+	switch choiceEdits {
+	case 1:
+		fmt.Println("Редактируем имя")
+		newName := GetInputString()
+		query = `
+		UPDATE tasks SET name = $1
+		WHERE id = $2
+		`
+		_, err = db.Exec(query, newName, id)
+		handleError(err, "Ошибка при обновлении имени:")
+		fmt.Printf("Задача с ID: %d редактирована\n", id)
+	case 2:
+		fmt.Println("Редактируем status")
+		fmt.Println("1. Выполнена")
+		fmt.Println("2. Не выполнена")
+		statusChoice := GetInputInt()
+		newStatus := statusChoice == 1
+		query = `
+		UPDATE tasks SET status = $1
+		WHERE id = $2
+		`
+		_, err = db.Exec(query, newStatus, id)
+		handleError(err, "Ошибка при обновлении статуса:")
+		fmt.Printf("Задача с ID: %d редактирована\n", id)
+	default:
+		fmt.Println("Неверный выбор. Попробуй еще раз")
 	}
 }
 
 func ShowTasks() {
-	fmt.Println("Задачи:")
-	for _, task := range taskList {
-		fmt.Printf("ID: %s\n", task.ID)
-		fmt.Printf("Name: %s\n", task.Name)
-		fmt.Printf("Status: %t\n", task.Status)
-	}
-}
-
-func SaveTasks(filename string) {
-	file, err := os.Create(filename)
-	if err != nil {
-		fmt.Println("Ошибка создания файла:", err)
-		return
-	}
-	var closeErr error
+	query := `
+	SELECT id, name, status FROM tasks
+	`
+	rows, err := db.Query(query)
+	handleError(err, "Ошибка при запросе задач:")
 	defer func() {
-		closeErr = file.Close()
-		if closeErr != nil {
-			fmt.Println("Ошибка при закрытии файла:", err)
-		}
-	}()
-	writer := bufio.NewWriter(file)
-	for _, task := range taskList {
-		line := task.ID + "," + task.Name + "," + strconv.FormatBool(task.Status) + "\n"
-		_, err = writer.WriteString(line)
-		if err != nil {
-			fmt.Println("Ошибка записи в файла: ", err)
-			return
-		}
-	}
-
-	err = writer.Flush()
-	if err != nil {
-		fmt.Println("Ошибка сохранения в файл: ", err)
-		return
-	}
-
-	fmt.Println("Данные успешно сохранены")
-}
-
-func LoadTasks(filename string) {
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Ошибка открытия файла:", err)
-		return
-	}
-	var closeErr error
-	defer func() {
-		closeErr = file.Close()
-		if closeErr != nil {
-			fmt.Println("Ошибка при закрытии файла:", err)
+		if err = rows.Close(); err != nil {
+			log.Printf("Ошибка при закрытии rows: %v", err)
 		}
 	}()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		pars := strings.Split(line, ",")
-		if len(pars) != 3 {
-			continue
-		}
-		id := pars[0]
-		name := pars[1]
-		status, _ := strconv.ParseBool(pars[2])
-		taskList = append(taskList,
-			Task{
-				ID:     id,
-				Name:   name,
-				Status: status,
-			})
+	fmt.Println("Задачи: ")
+	for rows.Next() {
+		var id int
+		var name string
+		var status bool
+		err = rows.Scan(&id, &name, &status)
+		handleError(err, "Ошибка с базой данных:")
+		fmt.Printf("ID: %d | Name: %s | Status: %t\n", id, name, status)
 	}
-	if err = scanner.Err(); err != nil {
-		fmt.Println("Ошибка при чтении файла:", err)
-	}
-	fmt.Println("Данные успешно заргужены")
 }
 
 func main() {
+	initDB()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("Ошибка при закрытии rows: %v", err)
+		}
+	}()
+	createTable()
 	for {
 		menu()
 		fmt.Print("Введите номер выбора: ")
@@ -264,10 +227,6 @@ func main() {
 		case 5:
 			ShowTasks()
 		case 6:
-			LoadTasks("tasks.txt")
-		case 7:
-			SaveTasks("tasks.txt")
-		case 8:
 			fmt.Println("Выход из программы")
 			return
 		default:
